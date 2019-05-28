5a1c2e478e45ecae5a32ed178cd282743dc89532
acdemiralp <demiralpali@gmail.com>
2019-05-28 15:54:43 +0200

    Load parallel HDF5.

--- utility/shell/fzj/load_common_modules.sh
+++ utility/shell/fzj/load_common_modules.sh
@@ -5,6 +5,6 @@ module load Intel/2019.3.199-GCC-8.3.0
 module load IntelMPI/2019.3.199
 
 module load Boost/1.69.0-Python-2.7.16
-module load HDF5/1.10.5-serial
+module load HDF5/1.10.5
 module load imkl/2019.3.199
 module load tbb/2019.4.199
