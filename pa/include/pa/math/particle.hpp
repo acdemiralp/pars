b79eb40b2647410e90cf2f203bc726534513cbc5
acdemiralp <demiralpali@gmail.com>
2019-07-05 14:37:28 +0200

    Particles now hold their original rank and voxel.

--- pa/include/pa/math/particle.hpp
+++ pa/include/pa/math/particle.hpp
@@ -16,17 +16,24 @@ struct PA_EXPORT particle
   template<class archive_type>
   void serialize(archive_type& archive, const std::uint32_t version)
   {
-    archive & position[0];
-    archive & position[1];
-    archive & position[2];
-    archive & position[3];
-    archive & remaining_iterations;
-    archive & vector_field_index  ;
+    archive & position            [0];
+    archive & position            [1];
+    archive & position            [2];
+    archive & position            [3];
+    archive & remaining_iterations   ;
+    archive & vector_field_index     ;
+    archive & original_rank          ;
+    archive & original_voxel      [0];
+    archive & original_voxel      [1];
+    archive & original_voxel      [2];
   }
 
-  vector4 position             = {};
-  integer remaining_iterations = 0 ;
-  integer vector_field_index   = 0 ;
+  vector4  position             = {};
+  integer  remaining_iterations = 0 ;
+  integer  vector_field_index   = 0 ;
+  
+  integer  original_rank        = 0 ;
+  ivector3 original_voxel       = {};
 };
 }
 
